0ad1d801af05386c2e0c73578ad4bc302c2b61a3
minheock <dygks0602@naver.com>
2023-08-14 12:31:36 +0900

    최신화

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/be/207057c84038001e1b14cabacb540f9e
@@ -0,0 +1,11 @@
+package practice_al.prac;
+
+
+public class Al12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- practice_al/src/practice_al/prac/Al11.java
+++ practice_al/src/practice_al/prac/Al11.java
@@ -1,28 +1,55 @@
 package practice_al.prac;
-public class Al11 {
 
+public class Al11 {
+	/* 조건 문자열 :
+	 * 두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과
+	 * m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+	 * 
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int result = solution(">","!", 41, 78);
+		int result = solution(">", "!", 41, 78);
 		System.out.println(result);
-		System.out.println(1<2);
+		System.out.println(1 < 2);
+	}
+
+	// ">", "!", 41, 78
+	public static int solution(String ineq, String eq, int n, int m) {
+		boolean whats = true;
+		if (ineq == "<" && eq == "=") {
+			whats = n <= m;
+		} else if (ineq == ">" && eq == "=") {
+			whats = n >= m;
+		} else if (ineq == "<" && eq == "!") {
+			whats = n < m;
+		} else if (ineq == ">" && eq == "!") {
+			whats = n > m;
+		}
+
+		if (whats) {
+			return 1;
+		} else
+			return 0;
 	}
 	
-	//	">", "!", 41, 78
-	  public static int solution(String ineq, String eq, int n, int m) {
-	        boolean whats = true;
-	        if(ineq == "<" && eq == "="){
-	           whats = n <= m;
-	        }else if(ineq == ">" && eq == "="){
-	           whats = n >= m;
-	        }else if(ineq == "<" && eq == "!"){
-	           whats = n < m;
-	        }else if(ineq == ">" && eq == "!"){
-	           whats = n > m;
-	        }
-	        
-	        if(whats){
-	            return 1;
-	        }else return 0;
-	    }
+	public static int solution2(String ineq, String eq, int n, int m) {
+		boolean whats = true;
+		if (ineq == "<" && eq == "=") {
+			whats = n <= m;
+		} else if (ineq == ">" && eq == "=") {
+			whats = n >= m;
+		} else if (ineq == "<" && eq == "!") {
+			whats = n < m;
+		} else if (ineq == ">" && eq == "!") {
+			whats = n > m;
+		}
+
+		if (whats) {
+			return 1;
+		} else
+			return 0;
+	}
+	
+	
+	
 }
